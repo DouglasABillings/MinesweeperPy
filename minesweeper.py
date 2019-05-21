4151f6625ed2f2d4f50e8ff024121698253b0850
DouglasABillings <douglasbillings91@gmail.com>
2019-05-21 18:39:07 -0500

    Added functions to render the board

--- minesweeper.py
+++ minesweeper.py
@@ -39,6 +39,33 @@ def create_game(board_width, board_height, num_bombs):
     }
 
 
+def cell_to_char(cell):
+    """ Generate a Char from a given Cell
+    :param cell:
+    :return: A Char representing the given cell
+    """
+    if cell["visible"]:
+        return cell["value"]
+    else:
+        return 'F' if cell["flagged"] else '#'
+
+
+def board_to_string(board):
+    """ Generates a string from a given board
+    :param board:
+    :return: A String representing the given board
+    """
+    board_string = ""
+    for y in range(len(board)):
+        row_string = ""
+        for x in range(len(board[0])):
+            row_string += cell_to_char(board[y][x])
+        board_string += row_string
+        if y in range(len(board) - 1):
+            board_string += '\n'
+    return board_string
+
+
 if __name__ == '__main__':
     def test_create_cell():
         # Can create a default cell
@@ -62,6 +89,7 @@ if __name__ == '__main__':
         another_cell = create_cell()
         assert another_cell == {"visible": False, "flagged": False, "value": '0'}
         assert a_cell == {"visible": False, "flagged": False, "value": "LOL"}
+    # Can create a cell
     test_create_cell()
 
     def test_create_board():
@@ -96,6 +124,7 @@ if __name__ == '__main__':
         board[0][1]["value"] = 'X'
         assert board == modified_board
         assert create_board(1, 1) == [[{"visible": False, "flagged": False, "value": '0'}]]
+    # Can create a board
     test_create_board()
 
     def test_create_game():
@@ -132,9 +161,61 @@ if __name__ == '__main__':
         # Can create a new game
         assert create_game(board_width=3, board_height=2, num_bombs=10) == game1
         assert create_game(board_width=2, board_height=1, num_bombs=3) == game2
+    # Can create a game
     test_create_game()
 
-    # TODO: convert a cell to a string
-    # TODO: convert a board to a string
+    def test_cell_to_char():
+        # Can convert a hidden cell to a char correctly
+        assert cell_to_char(create_cell()) == '#'
+        assert cell_to_char(create_cell(value='X')) == '#'
+        assert cell_to_char(create_cell(value='1')) == '#'
+
+        # Can convert a flagged cell to a char correctly
+        assert cell_to_char(create_cell(flagged=True)) == 'F'
+        assert cell_to_char(create_cell(value='X', flagged=True)) == 'F'
+        assert cell_to_char(create_cell(value='3', flagged=True)) == 'F'
+
+        # Can convert a visible cell to a char correctly
+        assert cell_to_char(create_cell(visible=True)) == '0'
+        assert cell_to_char(create_cell(visible=True, flagged=True)) == '0'
+        assert cell_to_char(create_cell(visible=True, value='X')) == 'X'
+        assert cell_to_char(create_cell(visible=True, value='1')) == '1'
+        assert cell_to_char(create_cell(visible=True, value='2')) == '2'
+        assert cell_to_char(create_cell(visible=True, value='3')) == '3'
+    # Can convert a cell to a char
+    test_cell_to_char()
+
+    def test_board_to_string():
+        test_board = [
+            [
+                {"visible": True, "flagged": False, "value": '0'},
+                {"visible": True, "flagged": False, "value": '0'},
+                {"visible": True, "flagged": False, "value": '1'},
+                {"visible": False, "flagged": True, "value": 'X'},
+                {"visible": True, "flagged": False, "value": '1'}
+            ],
+            [
+                {"visible": True, "flagged": False, "value": '1'},
+                {"visible": True, "flagged": False, "value": '1'},
+                {"visible": True, "flagged": False, "value": '1'},
+                {"visible": True, "flagged": False, "value": '1'},
+                {"visible": True, "flagged": False, "value": '1'}
+            ],
+            [
+                {"visible": True, "flagged": False, "value": 'X'},
+                {"visible": True, "flagged": False, "value": '1'},
+                {"visible": True, "flagged": False, "value": '0'},
+                {"visible": True, "flagged": False, "value": '0'},
+                {"visible": True, "flagged": False, "value": '0'}
+            ]
+        ]
+
+        # Can convert a board to a string correctly
+        assert board_to_string(create_board(5, 5)) == "#####\n#####\n#####\n#####\n#####"
+        assert board_to_string(create_board(3, 2)) == "###\n###"
+        assert board_to_string(create_board(5, 2)) == "#####\n#####"
+        assert board_to_string(test_board) == "001F1\n11111\nX1000"
+    # Can convert a board to a string
+    test_board_to_string()
 
     print("All tests passed!")
